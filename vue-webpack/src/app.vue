e616f2e171f94f891a18e0ab0c85893cd6e23ce3
nossikalin <nossikalin@tencent.com>
2019-04-19 18:39:27 +0800

    temp commit

---
+++ vue-webpack/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    {{ message }}
+  </section>
+</template>
+
+<script>
+
+import GreenTitle from 'components/green-title';
+
+export default {
+  data() {
+    return 'hello world';
+  }
+}
+</script>
+
+<style>
+
+</style>
