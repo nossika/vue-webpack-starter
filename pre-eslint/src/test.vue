47b51a05f8fb7d46eeb1435ad4446b64fed8e7b6
nossikalin <nossikalin@tencent.com>
2019-04-29 16:18:51 +0800

    pre-lint test

---
+++ pre-eslint/src/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    {{ msg }}
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hi',
+    };
+  },
+};
+</script>
+
+<style>
+.section {
+  color: #333;
+}
+</style>
